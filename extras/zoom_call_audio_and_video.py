9fa4a1be55848b97dddb2967c0b00856aa650bc4
Shields Lab <57649371+shieldslab@users.noreply.github.com>
2021-05-12 07:23:58 -0400

    Update zoom_call_audio_and_video.py

--- extras/zoom_call_audio_and_video.py
+++ extras/zoom_call_audio_and_video.py
@@ -7,11 +7,13 @@ kbd = Keyboard(usb_hid.devices)
 
 while True:
     if cpx.button_a:
+        # Alt-A is mute/unmute
         kbd.send(Keycode.ALT, Keycode.A)
         while cpx.button_a: # Wait for button to be released
             pass
 
     if cpx.button_b:
+        # Alt-V is video on/off
         kbd.send(Keycode.ALT, Keycode.V)
         while cpx.button_b: # Wait for button to be released
             pass
